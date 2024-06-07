a9651f4770222af466b9e7b2700ad91fc1591235
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-06-07 23:33:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
-function name(num1, num2){
+/* function name(num1, num2){
     console.log(num1 + num2);
 };
 
-name(1,"2")
+name(1,2)
+ */
+
+let user = { name: "lasith", age : 25}
+
+console.log(user.name)
