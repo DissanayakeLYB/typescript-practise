75eb341a079641a9900054a6d3415b637e39b9f0
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-07-18 08:33:27 +0530

    Create variables.js

---
+++ variables/variables.js
@@ -0,0 +1,16 @@
+// var can be initialized and reassigned as many times as possible
+var x = 30
+var x = 20
+x = 20
+
+// using let, will not allow to reassign using let again, but can be reassigned without any type
+let y = 30
+// let y = 25
+y = 23
+
+// const will not allow to update by any means
+const z = 20
+// const z = 10
+// z = 19
+
+
