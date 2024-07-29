586c38ad47dbbc253495e8c2c6d34c817ac9ad2c
Lasith Dissanayake <lasithdissanayake99@gmail.com>
2024-07-29 22:39:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 let user = { name: "lasith", age : 25}
 
 document.write(user.name)
+
+var games = "valo";
+
+
+function new_func(name, game){
+    document.write(name)
+    document.write(game)
+    document.write(games)
+}
+
+new_func("<br> Lasith", " <br> <br> CODM")
