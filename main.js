cfd7b72fa9591f5bc2dd69a33e2ec381a46c0764
Lasith Dissanayake <lasithdissanayake99@gmail.com>
2024-07-30 21:48:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,64 @@
-let user = { name: "lasith", age : 25}
-
-document.write(user.name)
+let user = { name: "lasith", age : 25};
 
 var games = "valo";
 
+const namdeshiyam = "LYB";
 
-function new_func(name, game){
-    document.write(name)
-    document.write(game)
-    document.write(games)
+//fucntion syntax
+function tell_name(name){
+    document.write(name);
 }
 
-new_func("<br> Lasith", " <br> <br> CODM")
+function tell_game(game){
+    document.write(game);
+}
+
+let a = 30;
+
+//if else 
+if (a<20){
+    tell_name(user.name);
+} else {
+    tell_game(games);
+}
+
+
+// conditional terniary operator
+var de = 100;
+var bw;
+
+de == 100 ? (bw = "Yes") : (bw = "No");
+
+document.write(bw)
+
+
+//switch case
+var day = 1;
+
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    case 4:
+        document.write("Thursday");
+        break;
+    case 5:
+        document.write("Friday");
+        break;
+    case 6:
+        document.write("Saturday");
+        break;
+    case 7:
+        document.write("Sunday");
+        break;
+
+    default:
+        document.write("The date you entered is wrong.")
+        break;
+}
