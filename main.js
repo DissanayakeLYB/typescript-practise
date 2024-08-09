07c5d7ee59ecb4b9b34ccfdc3ddec45d6f23a846
Lasith Dissanayake <lasithdissanayake99@gmail.com>
2024-08-09 15:11:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-let user = { name: "lasith", age : 25};
-
-var games = "valo";
-
-const namdeshiyam = "LYB";
-
-//fucntion syntax
-function tell_name(name){
-    document.write(name);
+function addition(a,b){
+    return (a+b);
 }
 
-function tell_game(game){
-    document.write(game);
-}
+var x = addition(31,342)
 
-let a = "2";
-let b = 1;
-
-if (a===b){
-    document.write("Values are equal and data types are equal")
-} else if (a==b){
-    document.write("values are equal but data type is different.")
-} else{
-    document.write("Values and data types are different.")
-}
+document.write(x)
