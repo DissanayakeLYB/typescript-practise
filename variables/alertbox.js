8d5e59e7bae09ad5650e70e4ec694b6f9b5eff9d
Lasith Dissanayake <lasithdissanayake99@gmail.com>
2024-08-09 14:52:46 +0530

    Create alertbox.js

---
+++ variables/alertbox.js
@@ -0,0 +1,20 @@
+// alert boxes
+alert("This is an alert message")
+
+// confirm boxes
+var c = confirm("Are you human?")
+
+if (c) {
+    alert("Welcome");
+} else {
+    alert("you are not welcome");
+}
+
+// prompt box
+var entrance = prompt("Password");
+
+if (entrance != "lasith"){
+    alert("palayan bn yanna.");
+} else {
+    alert("Welcome to my diary!");
+}
