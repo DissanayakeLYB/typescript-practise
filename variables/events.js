9d2c91270f47904f74f3f6fe2df76fc2e1555a83
Lasith Dissanayake <lasithdissanayake99@gmail.com>
2024-08-19 00:03:26 +0530

    Update events.js

--- variables/events.js
+++ variables/events.js
@@ -4,3 +4,9 @@ function HW(){
 
 
 <button onclick="HW()">Click Here</button>
+<button ondbclick="HW()">Click Here</button>
+<button oncontextmenu="HW()">Click Here</button>
+<button onmouseenter="HW()">Click Here</button>
+<button onkeypress="HW()">Click Here</button>
+<button onresize="HW()">Click Here</button>
+<button onscroll="HW()">Click Here</button>
